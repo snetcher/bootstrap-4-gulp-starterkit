0d172d7733c1a6f3cd70387e1ce1bee9c0f5160f
Constantine Tretyakoff <snetcher@gmail.com>
2017-06-21 13:39:21 +0300

    add node module gulp-autoprefixer

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
