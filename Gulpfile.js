a085037acc1d0b3e1b69d5b635b7c6aece79bcc9
Constantine Tretyakoff <snetcher@gmail.com>
2017-06-21 14:43:39 +0300

    add node module Rename
    for minimisation and rename file

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,13 +3,12 @@ var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var minifycss   = require('gulp-clean-css');
 var prefix      = require('gulp-autoprefixer');
+var rename      = require('gulp-rename');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
-    browserSync.init({
-        server: "./app"
-    });
+    browserSync.init({server: "./app"});
 
     gulp.watch("app/scss/*.scss", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
@@ -20,7 +19,9 @@ gulp.task('sass', function() {
     return gulp.src("app/scss/*.scss")
         .pipe(sass())
         .pipe(prefix("last 2 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(gulp.dest("app/css"))
         .pipe(minifycss())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
